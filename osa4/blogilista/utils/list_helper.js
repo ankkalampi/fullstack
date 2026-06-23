dc6aefea44693a1ed355d6d2fa628ee826291440
Paul Veltheim <paul.veltheim@gmail.com>
2026-06-24 01:39:09 +0300

    ex 4.6 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -12,13 +12,41 @@ const favoriteBlog = (blogs) => {
   }
 
   return blogs.reduce((favorite, blog) => {
-    
+
     return blog.likes > favorite.likes ? blog : favorite
   })
-} 
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const blogCounts = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) +1
+    return counts
+  }, {})
+
+  let maxWriter = null
+
+  let maxBlogs = 0
+
+  const writerList = blogCounts
+
+  for (const writer in writerList) {
+    if (writerList[writer] > maxBlogs) {
+        maxBlogs = writerList[writer]
+        maxWriter = writer
+    }
+  }
+
+  return {
+    author: maxWriter,
+    blogs: maxBlogs
+  }
+}
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
