9a0dd47b85193efef28a6c76304cad68d77c7b6d
Paul Veltheim <paul.veltheim@gmail.com>
2026-06-23 17:15:36 +0300

    ex 3.19 done

--- osa3/puhelinluettelo_backend/models/entry.js
+++ osa3/puhelinluettelo_backend/models/entry.js
@@ -16,8 +16,15 @@ mongoose.connect(url, { family: 4 })
     })
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    }
 })
 
 phonebookSchema.set('toJSON', {
